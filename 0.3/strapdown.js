5d9ff73250d64c86e292851b3a66d58f37cdc39c
Emma Strubell <strubell@cs.umass.edu>
2014-01-27 15:08:29 -0500

    try adding footer

--- 0.3/strapdown.js
+++ 0.3/strapdown.js
@@ -419,9 +419,9 @@ if("undefined"==typeof jQuery)throw new Error("Bootstrap requires jQuery");+func
   }
   
   // Insert footer
-//  var newNode = document.createElement('footer');
-//  newNode.innerHTML = '<hr><p>Factorie is open source software <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache License 2.0</a>. Please <a href=#cite>cite</a> for research use.<br><a href="http://www.iesl.cs.umass.edu/">Information Extraction and Synthesis Laboratory</a> (IESL), <a href="http://www.cs.umass.edu/">Computer Science Department</a>, <a href="http://www.umass.edu/">University of Massachusetts Amherst</a>.</p>'
-//  document.body.insertAfter(newNode, document.body.lastChild)
+  var newNode = document.createElement('footer');
+  newNode.innerHTML = '<hr><p>Factorie is open source software <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache License 2.0</a>. Please <a href=#cite>cite</a> for research use.<br><a href="http://www.iesl.cs.umass.edu/">Information Extraction and Synthesis Laboratory</a> (IESL), <a href="http://www.cs.umass.edu/">Computer Science Department</a>, <a href="http://www.umass.edu/">University of Massachusetts Amherst</a>.</p>'
+  document.body.insertAfter(newNode, document.body.lastChild)
 	  
   //////////////////////////////////////////////////////////////////////
   //
